ff061ee3b72b7931a6d80c1d94e19d8d078e2ee3
Simone <63718887+Srizza93@users.noreply.github.com>
2021-02-25 09:44:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+class Tool_tip {
+  
+    constructor() {
+        this.tool_tip = document.querySelector(".tool_tip_text");
+        this.toolTip_width = this.tool_tip.offsetWidth;
+        this.calculations();
+    }
 
+    rendering() {
+        this.tool_tip.style.marginLeft = this.toolTip_width + 'px';
+    }
+
+    calculations() {
+        this.toolTip_width = - this.toolTip_width / 2;
+        this.rendering();
+    }
+}
+
+new Tool_tip();
