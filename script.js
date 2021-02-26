0b48aa37d7da35d9b18f884468ad81404b3c9c69
Simone <63718887+Srizza93@users.noreply.github.com>
2021-02-26 23:42:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,40 @@
 class Tool_tip {
-    
     constructor() {
-        this.toolTip = document.getElementsByClassName("tool_tip");
-        this.toolTip_text = null;
+        this.toolTip = document.querySelectorAll("div.tool_tip");
+        this.toolTip_text = undefined;
         this.text = "200 x 200";
         this.text_marginLeft = 0;
+        this.textWidth = "";
         this.loop();
-    }
-    
-    // Assign class to the new span and center it to the div
-    rendering() {
-        this.toolTip_text.className += "tool_tip_text";
-        this.calculations();
-        this.toolTip_text.style.marginLeft = - this.text_marginLeft + 'px';
-    }
-    
-    // Create the new span and append it to the tool_tip class elements
-    createNewElement(div) {
-        this.toolTip_text = document.createElement("span");
-        this.toolTip_text.appendChild(document.createTextNode(this.text));
-        div.appendChild(this.toolTip_text);
-        this.rendering();
-    }
-    
-    calculations() {
-        this.text_marginLeft = this.toolTip_text.offsetWidth / 2;
     }
     
     // Loop through all divs in the page with tool_tip class name
     loop() {
         for (let i = 0; i < this.toolTip.length; i++) {
-            this.createNewElement(this.toolTip[i]);
+            this.createNewElement(this.toolTip[i],i);
         }
     }
+    
+    // Create the new span and append it to the tool_tip class elements
+    createNewElement(div,i) {
+        this.toolTip_text = document.createElement("span");
+        this.toolTip_text.appendChild(document.createTextNode(this.text + " " + i));
+        div.appendChild(this.toolTip_text);
+        this.rendering();
+    }
+    
+    // Assign class to the new span and center it to the div
+    rendering() {
+        this.toolTip_text.className = "tool_tip_text";
+        console.log(this.toolTip_text.offsetWidth);
+        this.calculations();
+        this.toolTip_text.style.marginLeft = - this.text_marginLeft + 'px';
+    }
+    
+    calculations() {
+        this.text_marginLeft = this.toolTip_text.offsetWidth / 2;
+    }
+
 }
 
 new Tool_tip();
