6816deb81c60b354e1b272d4de325d8e193c0961
Simone <63718887+Srizza93@users.noreply.github.com>
2021-03-03 00:17:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,35 +2,30 @@ class ToolTip {
     
     constructor() {
         this.toolTip = document.querySelectorAll('[data-tooltip]');
+        this.toolTipWrapper = undefined;
         this.toolTipPopUp = undefined;
-        this.marginLeftPopUp = 0;
         this.findAllTooltips();
     }
     
     // Loop through all elements in the page with data-tooltip property
     findAllTooltips() {
-        this.toolTip.forEach((container) => {
-            container.addEventListener("onload", this.render(container));
-        });
+        this.toolTip.forEach((container) => { this.render(container) });
     }
     
     render(container) {
         this.textPopUp = container.getAttribute('data-tooltip');
-        this.toolTipPopUp = document.createElement("span");
+        this.toolTipWrapper = document.createElement('span');
+        this.toolTipPopUp = document.createElement('span');
+        this.toolTipWrapper.appendChild(this.toolTipPopUp);
         this.toolTipPopUp.appendChild(document.createTextNode(this.textPopUp));
-        container.className = "toolTip";
-        this.toolTipPopUp.className = "toolTipPopUp";
+        container.classList.add('toolTip');
+        this.toolTipWrapper.classList.add('toolTipWrapper');
+        this.toolTipPopUp.classList.add('toolTipPopUp');
         this.mount(container);
     }
     
     mount(tooltip) {
-        tooltip.appendChild(this.toolTipPopUp);
-        this.position();
-    }
-    
-    position() {
-        this.toolTipPopUp.style.transform = "translateX(-50%)";
-        this.toolTipPopUp.style.left = "50%";
+        tooltip.appendChild(this.toolTipWrapper);
     }
 
 }
